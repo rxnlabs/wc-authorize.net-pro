4312f3ed4b25b1b61bafbc37d2ba4c5aaf746fc5
mohsinoffline <mohsinoffline@gmail.com>
2020-05-29 15:58:34 +0530

    Fixed order line items

--- includes/class-wc-gateway-authnet.php
+++ includes/class-wc-gateway-authnet.php
@@ -310,14 +310,15 @@ class WC_Gateway_Authnet extends WC_Payment_Gateway_CC {
 			);
 
             $line_items = array();
-
 			foreach ( $order->get_items() as $id => $item ) {
 				$product = $item->get_product();
-
-				$line_item['id'] = is_object( $product ) && $product->get_sku() ? substr( $product->get_sku(), 0, 31 ) : substr( $product->get_id(), 0, 31 );
-				$line_item['name'] = substr( $this->format_line_item( $item['name'] ), 0, 31 );
+                if( !is_object( $product ) ) {
+                    continue;
+                }
+				$line_item['id'] = $product->get_sku() ? substr( $product->get_sku(), 0, 31 ) : substr( $product->get_id(), 0, 31 );
+				$line_item['name'] = substr( $this->format_line_item( $item->get_name() ), 0, 31 );
 				$line_item['description'] = '';
-				$line_item['quantity'] = $item['qty'];
+				$line_item['quantity'] = $item->get_quantity();
 				$line_item['unit_price'] = $order->get_item_total( $item );
 				$line_item['taxable'] = $product->is_taxable();
 
@@ -392,7 +393,7 @@ class WC_Gateway_Authnet extends WC_Payment_Gateway_CC {
             $this->log( sprintf( __( 'Gateway Error: %s', 'wc-authnet' ), $e->getMessage() ) );
             if( is_wp_error( $response ) ) {
                 $response = $response->get_error_data();
-                $order->add_order_note( sprintf( __( 'Authorize.Net response reason: %s', 'wc-authnet' ), $response['response_reason_text'] ) );
+                $order->add_order_note( sprintf( __( 'Authorize.Net failure reason: %s', 'wc-authnet' ), $response['response_reason_text'] ) );
             }
 
 			do_action( 'wc_gateway_authnet_process_payment_error', $e, $order );
