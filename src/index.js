71d3c3d306a41afc631e438bd7a4e2162720814f
mohsinoffline <mohsinoffline@gmail.com>
2023-12-30 01:57:33 -0800

    added support for block checkout

---
+++ src/index.js
@@ -0,0 +1,147 @@
+import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { __ } from '@wordpress/i18n';
+import { css } from 'styled-components';
+import { PaymentInputsWrapper, usePaymentInputs } from 'react-payment-inputs';
+import images from 'react-payment-inputs/images';
+import { useEffect, useState } from '@wordpress/element';
+import { getBlocksConfiguration } from './utils';
+import { usePaymentProcessing } from './payment-processing';
+import './style.scss';
+
+const PAYMENT_METHOD_NAME = 'authnet';
+
+const getCreditCardIcons = () => {
+	return Object.entries( getBlocksConfiguration()?.icons ?? {} ).map(
+		( [ id, { src, alt } ] ) => {
+			return {
+				id,
+				src,
+				alt,
+			};
+		}
+	);
+};
+
+const Label = ( props ) => {
+	const { PaymentMethodLabel } = props.components;
+
+	const labelText =
+		getBlocksConfiguration()?.title ??
+		__( 'Credit / Debit Card', 'wc-authnet' );
+
+	return <PaymentMethodLabel text={ labelText } />;
+};
+
+const CreditCardComponent = ( {
+								  billing,
+								  eventRegistration,
+								  emitResponse,
+								  components
+							  } ) => {
+	const { onPaymentSetup, onCheckoutAfterProcessingWithError } = eventRegistration;
+	const [ ccError, setCCError ] = useState(null);
+	const [cardNumber, setCardNumber] = useState("");
+	const [expiryDate, setExpiryDate] = useState("");
+	const [cvc, setCVC] = useState("");
+	const { PaymentMethodIcons } = components;
+
+	const onAuthnetError = usePaymentProcessing(
+		billing,
+		cardNumber,
+		expiryDate,
+		cvc,
+		PAYMENT_METHOD_NAME,
+		emitResponse,
+		onPaymentSetup,
+		onCheckoutAfterProcessingWithError
+	);
+
+
+	const onCCError = ( error, erroredInputs ) => {
+		//console.log(error);
+		//console.log(erroredInputs);
+		setCCError(error);
+		onAuthnetError(error);
+		return true;
+	};
+
+	const cardNumberValidator = ({ cardNumber, cardType, errorMessages }) => {
+		if(getBlocksConfiguration()?.allowed_card_types.indexOf(cardType.type) >= 0) {
+			return;
+		}
+		//console.log(cardType.type, getBlocksConfiguration()?.allowed_card_types);
+		if(cardType.type === 'dinersclub' && getBlocksConfiguration()?.allowed_card_types.indexOf('diners-club') >= 0) {
+			return;
+		}
+		return getBlocksConfiguration()?.card_disallowed_error;
+	}
+
+	const ERROR_MESSAGES = {
+		emptyCardNumber: getBlocksConfiguration()?.no_card_number_error,
+		invalidCardNumber: getBlocksConfiguration()?.card_number_error,
+		emptyExpiryDate: getBlocksConfiguration()?.no_card_expiry_error,
+		monthOutOfRange: getBlocksConfiguration()?.card_expiry_error,
+		yearOutOfRange: getBlocksConfiguration()?.card_expiry_error,
+		dateOutOfRange: getBlocksConfiguration()?.card_expiry_error,
+		invalidExpiryDate: getBlocksConfiguration()?.card_expiry_error,
+		emptyCVC: getBlocksConfiguration()?.no_cvv_error,
+		invalidCVC: getBlocksConfiguration()?.card_cvc_error
+	};
+
+	const {
+		wrapperProps,
+		getCardImageProps,
+		getCardNumberProps,
+		getExpiryDateProps,
+		getCVCProps,
+		meta
+	} = usePaymentInputs({ cardNumberValidator, onError: onCCError, errorMessages: ERROR_MESSAGES });
+
+	const cardIcons = getCreditCardIcons();
+
+	const renderedCardElement = (
+		<div className="wc-block-gateway-container wc-inline-card-element wc-block-authnet-gateway-container">
+			<PaymentInputsWrapper {...wrapperProps}>
+				<svg {...getCardImageProps({ images })} />
+				<input {...getCardNumberProps({ onChange: e => setCardNumber(e.target.value) })} />
+				<input {...getExpiryDateProps({ onChange: e => setExpiryDate(e.target.value) })} />
+				<input {...getCVCProps({ onChange: e => setCVC(e.target.value) })} />
+			</PaymentInputsWrapper>
+		</div>
+	)
+	return (
+		<>
+			{ renderedCardElement }
+			{ PaymentMethodIcons && cardIcons.length && (
+				<PaymentMethodIcons icons={ cardIcons } align="left" />
+			) }
+		</>
+	);
+};
+
+function AuthnetCreditCard ( props ) {
+	const { authnet } = props;
+
+	return (
+		<CreditCardComponent { ...props } />
+	);
+}
+
+const authnetCcPaymentMethod = {
+	name: PAYMENT_METHOD_NAME,
+	label: <Label />,
+	content: <AuthnetCreditCard />,
+	edit: <AuthnetCreditCard />,
+	canMakePayment: () => true,
+	ariaLabel: __(
+		'Authnet payment method',
+		'wc-authnet'
+	),
+	supports: {
+		// Use `false` as fallback values in case server provided configuration is missing.
+		showSavedCards: getBlocksConfiguration()?.showSavedCards ?? false,
+		showSaveOption: getBlocksConfiguration()?.showSaveOption ?? false,
+		features: getBlocksConfiguration()?.supports ?? [],
+	},
+};
+registerPaymentMethod( authnetCcPaymentMethod );
---
+++ src/utils.js
@@ -0,0 +1,50 @@
+import {getSetting} from '@woocommerce/settings';
+
+export const getBlocksConfiguration = () => {
+	const authnetServerData = getSetting( 'authnet_data', null );
+
+	if ( ! authnetServerData ) {
+		throw new Error( 'nMI initialization data is not available' );
+	}
+
+	return authnetServerData;
+};
+
+export const getLoginID = () => {
+	const loginID = getBlocksConfiguration()?.login_id;
+	if ( ! loginID ) {
+		throw new Error(
+			'There is no Login ID available for Authorize.Net. Make sure it is available on the wc.authnet_data.login_id property.'
+		);
+	}
+	return loginID;
+};
+
+export const getClientKey = () => {
+	return getBlocksConfiguration()?.client_key;
+};
+
+export const authnetDispatch = async ( paymentData ) => {
+
+	const secureData = {
+		authData: {
+			apiLoginID: getLoginID(),
+			clientKey: getClientKey()
+		},
+		cardData: paymentData
+	};
+
+	return new Promise((resolve, reject) => {
+		if (window) {
+			window.Accept.dispatchData(
+				secureData,
+				(response) => {
+					if (response.messages.resultCode === 'Ok') {
+						resolve(response);
+					}
+					reject(response);
+				}
+			);
+		}
+	});
+};
