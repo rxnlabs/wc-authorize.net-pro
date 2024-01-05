8934be14702951b0a2dbb64bf3991f768bf75613
mohsinoffline <mohsinoffline@gmail.com>
2024-01-05 01:23:32 -0800

    changed more URLs to Woo.com

--- includes/class-wc-authnet-privacy.php
+++ includes/class-wc-authnet-privacy.php
@@ -73,7 +73,7 @@ class WC_Authnet_Privacy extends WC_Abstract_Privacy {
      *
      */
     public function get_privacy_message() {
-        return wpautop( sprintf( __( 'By using this extension, you may be storing personal data or sharing data with an external service. <a href="%s" target="_blank">Learn more about how this works, including what you may want to include in your privacy policy.</a>', 'wc-authnet' ), 'https://docs.woocommerce.com/document/privacy-payments/' ) );
+        return wpautop( sprintf( __( 'By using this extension, you may be storing personal data or sharing data with an external service. <a href="%s" target="_blank">Learn more about how this works, including what you may want to include in your privacy policy.</a>', 'wc-authnet' ), 'https://woo.com/document/privacy-payments/' ) );
     }
 
     /**
